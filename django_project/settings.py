5a07ff7cf307f9fe5f3380ed87ade9a347bfae26
farzanfaisal77 <farzanfaisal77@gmail.com>
2026-05-25 14:29:31 +0530

    secret key hiding

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,17 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os  
 from pathlib import Path
+from dotenv import load_dotenv
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load the hidden .env file
+load_dotenv(BASE_DIR / '.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wld7_z0d29kjveei5jkau&fjvkbgql24k-#8lu%0m_d$x_ywue"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
